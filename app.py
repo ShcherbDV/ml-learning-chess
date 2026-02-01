c235eda20538133536b51a694e3703564782cf6f
dmytro_shcherbyna <shcherbdmytro@gmail.com>
2026-02-01 18:58:57 +0100

    update app.py -> fix the bugs, handle 6 classes not just 2

--- app.py
+++ app.py
@@ -17,17 +17,25 @@ def home():
     return "<p>Hello, please go to '/classify' to predict chess figure</p>"
 
 
-app.post("/classify")
+@app.post("/classify")
 def upload_file():
+    if "file" not in request.files:
+        return {"error": "No file in request"}, 400
+
     file = request.files["file"]
+
+    if file.filename == "":
+        return {"error": "No selected file"}, 400
+
     upload_image_path = os.path.join(UPLOAD_FOLDER, file.filename)
     file.save(upload_image_path)
 
-    label, prob = classify(cnn_model, upload_image_path)
+    label, confidence = classify(cnn_model, upload_image_path)
 
-    prob = round(prob * 100, 2)
-
-    return {"label": label, "probability": prob}
+    return {
+        "figure": label,
+        "confidence": round(confidence * 100, 2),
+    }
 
 
 if __name__ == '__main__':
